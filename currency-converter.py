b87677e47c041df5174fb669493337343ab74421
Salma Momani <salmatechno77@gmail.com>
2026-09-23 04:52:01 +0300

    Add files via upload

---
+++ currency-converter.py
@@ -0,0 +1,78 @@
+#كود لتحويل مبلغ من الدينار الاردني الى اليورو
+#ادخال المستخدم قيمةالمبلغ الذي يريد تحويله
+currency=float(input("enter a value in JD"))
+#ادخال المستخدم سعر الصرف للعملة التي يريد التحويل اليها
+currency_eur=float(input("enter the rate of the eur"))
+#ضرب المبلغ بسعر الصرف لاخراج المبلغ المحول
+total=currency*currency_eur
+#حساب العمولة بضرب المبلغ ب 3 بالمئة
+commission=currency*0.03
+#قمت بعمل التحسين الاول بعرض المبلغ المحول دون حساب العمولة اولاً ليعرف المستخدم المبلغ قبل وبعد اضافة العمولة
+#طباعة المبلغ دون حساب العمولة
+print("the cost without commission",total)
+#طباعة المبلغ المحول كاملاً
+print("the cost with commission",commission+total)
+
+
+#كود لتحويل مبلغ من الدينار الاردني الى الدولار الامريكي
+#ادخال المستخدم قيمةالمبلغ الذي يريد تحويله
+currency=float(input("enter a value in JD"))
+#ادخال المستخدم سعر الصرف للعملة التي يريد التحويل اليها
+currency_usd=float(input("enter the rate of the usd"))
+#ضرب المبلغ بسعر الصرف لاخراج المبلغ المحول
+total=currency*currency_usd
+#حساب العمولة بضرب المبلغ ب 3 بالمئة
+commission=currency*0.03
+#قمت بعمل التحسين الاول بعرض المبلغ المحول دون حساب العمولة اولاً ليعرف المستخدم المبلغ قبل وبعد اضافة العمولة
+#طباعة المبلغ دون حساب العمولة
+print("the cost without commission",total)
+#طباعة المبلغ المحول كاملاً
+print("the cost with commission",commission+total)
+
+
+#كود لتحويل مبلغ من الدينار الاردني الى اليين الياباني
+#ادخال المستخدم قيمةالمبلغ الذي يريد تحويله
+currency=float(input("enter a value in JD"))
+#ادخال المستخدم سعر الصرف للعملة التي يريد التحويل اليها
+currency_jpy=float(input("enter the rate of the jpy"))
+#ضرب المبلغ بسعر الصرف لاخراج المبلغ المحول
+total=currency*currency_jpy
+#حساب العمولة بضرب المبلغ ب 3 بالمئة
+commission=currency*0.03
+#قمت بعمل التحسين الاول بعرض المبلغ المحول دون حساب العمولة اولاً ليعرف المستخدم المبلغ قبل وبعد اضافة العمولة
+#طباعة المبلغ دون حساب العمولة
+print("the cost without commission",total)
+#طباعة المبلغ المحول كاملاً
+print("the cost with commission",commission+total)
+
+
+#كود لتحويل مبلغ من الدينار الاردني الى الليرة التركي
+#ادخال المستخدم قيمةالمبلغ الذي يريد تحويله
+currency=float(input("enter a value in JD"))
+#ادخال المستخدم سعر الصرف للعملة التي يريد التحويل اليها
+currency_tur=float(input("enter the rate of the tur"))
+#ضرب المبلغ بسعر الصرف لاخراج المبلغ المحول
+total=currency*currency_tur
+#حساب العمولة بضرب المبلغ ب 3 بالمئة
+commission=currency*0.03
+#قمت بعمل التحسين الاول بعرض المبلغ المحول دون حساب العمولة اولاً ليعرف المستخدم المبلغ قبل وبعد اضافة العمولة
+#طباعة المبلغ دون حساب العمولة
+print("the cost without commission",total)
+#طباعة المبلغ المحول كاملاً
+print("the cost with commission",commission+total)
+
+
+#كود لتحويل مبلغ من الدينار الاردني الى الدرهم الاماراتي
+#ادخال المستخدم قيمةالمبلغ الذي يريد تحويله
+currency=float(input("enter a value in JD"))
+#ادخال المستخدم سعر الصرف للعملة التي يريد التحويل اليها
+currency_tur=float(input("enter the rate of the aed"))
+#ضرب المبلغ بسعر الصرف لاخراج المبلغ المحول
+total=currency*currency_tur
+#حساب العمولة بضرب المبلغ ب 3 بالمئة
+commission=currency*0.03
+#قمت بعمل التحسين الاول بعرض المبلغ المحول دون حساب العمولة اولاً ليعرف المستخدم المبلغ قبل وبعد اضافة العمولة
+#طباعة المبلغ دون حساب العمولة
+print("the cost without commission",total)
+#طباعة المبلغ المحول كاملاً
+print("the cost with commission",commission+total)
